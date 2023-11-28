da1877df0c7743e0675f5be22123f185ed6b256c
Máté Mustó <mustomate7@gmail.com>
2023-11-28 21:50:49 +0100

    update config

--- app/config/config.php
+++ app/config/config.php
@@ -1,16 +1,13 @@
 <?php
-// DB Params
+// Database
 define('DB_HOST', 'localhost');
 define('DB_USER', 'szoftverleltar');
 define('DB_PASS', 'szoftverleltar');
 define('DB_NAME', 'szoftverleltar');
 
-// App Root
+
 define('APPROOT', dirname(dirname(__FILE__)));
-// URL Root
 define('URLROOT', 'http://mustomate.nhely.hu/');
-// Site Name
 define('SITENAME', 'BestSite');
-// App Version
 define('APPVERSION', '1.7');
 
